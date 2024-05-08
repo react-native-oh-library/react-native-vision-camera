d703bf86c6453463147c1774e8b1d33c924d596e
hzjzwcu <1127781044@qq.com>
2024-05-08 18:01:28 +0800

    新增常用属性

--- package/src/RTNVisionCameraNativeComponent.ts
+++ package/src/RTNVisionCameraNativeComponent.ts
@@ -1,12 +1,26 @@
 import type { ViewProps } from "react-native/Libraries/Components/View/ViewPropTypes";;
 import type { HostComponent } from "react-native";
 import codegenNativeComponent from "react-native/Libraries/Utilities/codegenNativeComponent";
-import { Int32, WithDefault } from "react-native/Libraries/Types/CodegenTypes";
-
+import { Float, Int32, WithDefault } from "react-native/Libraries/Types/CodegenTypes";
+import type { CameraRuntimeError } from './types/CameraError'
+import type { CameraDevice } from './types/CameraDevice'
+import type { CodeScanner } from './types/CodeScanner'
 export interface NativeProps extends ViewProps {
   fps?: WithDefault<Int32, 30>;
-  isActive?: WithDefault<boolean, false>;
-  preview?: WithDefault<boolean, false>;
+  isActive: boolean;
+  preview?: WithDefault<boolean, true>;
+  device?: CameraDevice;
+  resizeMode?: WithDefault<'cover' | 'contain', 'cover'>;
+  enableZoomGesture?: WithDefault<boolean, false>;
+  codeScanner?: CodeScanner;
+  exposure?: WithDefault<Int32, 0>;
+  zoom?: WithDefault<Float, 1.0>;
+  audio?: WithDefault<boolean, false>;
+  torch?: WithDefault<'off' | 'on', 'off'>;
+  onStarted?: () => void;
+  onStopped?: () => void;
+  onInitialized?: () => void;
+  onError?: (error: CameraRuntimeError) => void;
 }
 
 export default codegenNativeComponent<NativeProps>(
