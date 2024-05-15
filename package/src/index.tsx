846f670123675db25c908a02699260382209a3fb
hzjzwcu <1127781044@qq.com>
2024-05-15 17:51:37 +0800

    更新入口组件

--- package/src/index.tsx
+++ package/src/index.tsx
@@ -1,85 +1,193 @@
+import React, {
+    useCallback,
+    useRef,
+    forwardRef,
+    useImperativeHandle,
+} from "react";
+import {
+    View,
+    StyleSheet,
+} from "react-native";
+import NativeVisionCameraView, { VisionCameraCommands } from "./NativeVisionCameraView";
+import type { VisionCameraCommandsType, VisionCameraComponentType } from "./NativeVisionCameraView";
 
-import { View } from "react-native";
-import React, { forwardRef, memo } from "react";
-import type { NativeVisionCameraProps } from './NativeVisionCameraView';
-import RTNVisionCamera from './NativeVisionCameraView';
-export * from './hooks/useCameraDevice';
-export * from './hooks/useCameraDevices';
-export * from './hooks/useCameraFormat';
-export * from './hooks/useCameraPermission';
-export * from './hooks/useCodeScanner';
-export * from './types/CodeScanner';
+import type { NativeSyntheticEvent } from "react-native";
+import { PhotoFile } from "./types/PhotoFile";
+import { VisionCameraProps } from "./types/Camera";
+import { Double } from "react-native/Libraries/Types/CodegenTypes";
 
-export interface PhotoFile {
-    width: number
-    height: number
+export interface VisionCameraRef extends Omit<VisionCameraCommandsType, 'focus' | 'takePhoto'> {
+    takePhoto: () => Promise<PhotoFile>;
+    focus: (x: Double, y: Double) => Promise<void>;
+    startRecording: () => void;
+    stopRecording: () => void;
+    pauseRecording: () => void;
+    resumeRecording: () => void;
+    cancelRecording: () => void;
 }
 
-function VisionCameraViewBase({
-    style,
-    children,
-    forwardedRef,
-    codeScanner,
-    fps,
-    isActive,
-    preview,
-    device,
-    resizeMode,
-    enableZoomGesture,
-    exposure,
-    zoom,
-    audio,
-    video,
-    torch,
-    onStarted,
-    onStopped,
-    onInitialized,
-    onError,
-    ...props
-}: NativeVisionCameraProps & { forwardedRef: React.Ref<any> }) {
-    return (
-        <View style={[style]} ref={forwardedRef}>
-            <RTNVisionCamera
-                {...props}
-                codeScanner={codeScanner}
-                fps={fps}
-                isActive={isActive}
-                preview={preview}
-                device={device}
-                resizeMode={resizeMode}
-                enableZoomGesture={enableZoomGesture}
-                exposure={exposure}
-                zoom={zoom}
-                audio={audio}
-                video={video}
-                torch={torch}
-                onStarted={onStarted}
-                onStopped={onStopped}
-                onInitialized={onInitialized}
-                onError={onError}
-            />
-            {children}
-        </View>
-    )
-}
+export const Camera = forwardRef<VisionCameraRef, VisionCameraProps>(
+    (
+        {
+            style,
+            device,
+            isActive,
+            preview,
+            resizeMode,
+            fps,
+            enableZoomGesture,
+            codeScanner,
+            format,
+            exposure,
+            zoom,
+            audio,
+            video,
+            torch,
+            onTouchEnd,
+            onStarted,
+            onStopped,
+            onInitialized,
+            onError,
+            ...rest
+        },
+        ref
+    ) => {
+        const VisionCameraRef = useRef<React.ElementRef<VisionCameraComponentType>>(null);
 
-const CameraMemo = memo(VisionCameraViewBase)
+        const takePhoto = useCallback(
+            () => {
+                if (!VisionCameraRef.current) throw new Error("VisionCameraRef.current is NaN");
+                return VisionCameraCommands.takePhoto(VisionCameraRef.current);
+            },
+            []
+        );
 
-const CameraComponent: React.ComponentType<NativeVisionCameraProps> = forwardRef(
-    (props: NativeVisionCameraProps, ref: React.Ref<any>) => (
-        <CameraMemo forwardedRef={ref} {...props} />
-    ),
-)
+        const focus = useCallback(
+            (x: Double, y: Double) => {
+                if (isNaN(x) || isNaN(y)) throw new Error("VisionCameraCommands focus point x or y is NaN");
+                if (!VisionCameraRef.current) throw new Error("VisionCameraRef.current is NaN");
+                return VisionCameraCommands.focus(VisionCameraRef.current, x, y);
+            },
+            []
+        );
 
-CameraComponent.displayName = 'Camera'
+        const startRecording = useCallback(
+            () => {
+                if (!VisionCameraRef.current) throw new Error("VisionCameraRef.current is NaN");
+                return VisionCameraCommands.startRecording(VisionCameraRef.current);
+            },
+            []
+        );
 
-export interface CameraStaticProperties {
-}
+        const stopRecording = useCallback(
+            () => {
+                if (!VisionCameraRef.current) throw new Error("VisionCameraRef.current is NaN");
+                return VisionCameraCommands.stopRecording(VisionCameraRef.current);
+            },
+            []
+        );
 
-interface CameraMethods {
-}
+        const pauseRecording = useCallback(
+            () => {
+                if (!VisionCameraRef.current) throw new Error("VisionCameraRef.current is NaN");
+                return VisionCameraCommands.pauseRecording(VisionCameraRef.current);
+            },
+            []
+        );
+
+        const resumeRecording = useCallback(
+            () => {
+                if (!VisionCameraRef.current) throw new Error("VisionCameraRef.current is NaN");
+                return VisionCameraCommands.resumeRecording(VisionCameraRef.current);
+            },
+            []
+        );
+
+        const cancelRecording = useCallback(
+            () => {
+                if (!VisionCameraRef.current) throw new Error("VisionCameraRef.current is NaN");
+                return VisionCameraCommands.cancelRecording(VisionCameraRef.current);
+            },
+            []
+        );
 
 
-export const Camera: React.ComponentType<NativeVisionCameraProps> & CameraStaticProperties & CameraMethods = CameraComponent as any
+        const onVisionCameraStarted = useCallback(
+            () => {
+                onStarted?.();
+            },
+            [onStarted]
+        );
+
+        const onVisionCameraStopped = useCallback(
+            () => {
+                onStopped?.();
+            },
+            [onStopped]
+        );
+
+        const onVisionCameraInitialized = useCallback(
+            () => {
+                onInitialized?.();
+            },
+            [onInitialized]
+        );
+
+        const onVisionCameraError = useCallback(
+            (error: NativeSyntheticEvent<{ error: string }>) => {
+                onError?.(error.nativeEvent);
+            },
+            [onError]
+        );
+
+        useImperativeHandle(
+            ref,
+            () => ({
+                focus,
+                takePhoto,
+                startRecording,
+                stopRecording,
+                pauseRecording,
+                resumeRecording,
+                cancelRecording,
+            }),
+            [
+                focus,
+                takePhoto,
+                startRecording,
+                stopRecording,
+                pauseRecording,
+                resumeRecording,
+                cancelRecording,
+            ]
+        );
+
+        return (
+            <View style={style}>
+                <NativeVisionCameraView
+                    ref={VisionCameraRef}
+                    style={StyleSheet.absoluteFill}
+                    codeScanner={codeScanner}
+                    fps={fps}
+                    isActive={false}
+                    preview={preview}
+                    device={device}
+                    resizeMode={resizeMode}
+                    enableZoomGesture={enableZoomGesture}
+                    exposure={exposure}
+                    zoom={zoom}
+                    audio={audio}
+                    video={video}
+                    torch={torch}
+                    onStarted={onVisionCameraStarted}
+                    onStopped={onVisionCameraStopped}
+                    onInitialized={onVisionCameraInitialized}
+                    onError={onVisionCameraError}
+                    {...rest}
+                />
+            </View>
+        );
+    }
+);
 
 export default Camera;
