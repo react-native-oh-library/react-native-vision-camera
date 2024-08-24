3cf5afc6a911e0c39cd7270ed5552d329846d47d
“leehom6666” <llh_01129@163.com>
2024-08-24 19:21:41 +0800

    fix:修改resizeMode为contain时宽高不对问题
    Signed-off-by: “leehom6666” <llh_01129@163.com>

--- package/harmony/vision_camera/src/main/ets/service/CameraSession.ts
+++ package/harmony/vision_camera/src/main/ets/service/CameraSession.ts
@@ -299,7 +299,14 @@ export default class CameraSession {
     this.cameraInput.open();
     this.capability = this.cameraManager.getSupportedOutputCapability(currentDevice, camera.SceneMode.NORMAL_PHOTO);
     if (surfaceId && props.preview) {
-      this.previewProfile = this.capability.previewProfiles[this.capability.previewProfiles.length - 1];
+      //this.previewProfile = this.capability.previewProfiles[this.capability.previewProfiles.length - 1];
+      this.previewProfile = {
+        format:1003,
+        size:{
+          width:1920,
+          height:1080
+        }
+      }
       Logger.info(TAG, `initPhotoSession createCameraInput ${JSON.stringify(this.previewProfile)})`);
       this.previewOutput = this.cameraManager.createPreviewOutput(this.previewProfile, surfaceId);
     }
